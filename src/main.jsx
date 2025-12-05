c5d7b94a713e02aaa67cc0fedfa098a6495eaddc
Bilgehan Kaplan <b0812064@gmail.com>
2025-12-05 21:07:34 +0100

    Finalize Vite/React structure for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './globals.css'; // BURASI ÇOK ÖNEMLİ: CSS dosyasını import etmeli
+import './global.css'; // BURASI ÇOK ÖNEMLİ: CSS dosyasını import etmeli
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
